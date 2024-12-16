06623fa87089957ad5cd1c1f3a5d5ad3f3329456
dhtns818 <dhtns818@rosesystems.kr>
2024-12-16 11:19:58 +0900

    commit

--- for-model/eventcollections/src/main/java/com/example/template/infra/EventCollectorViewHandler.java
+++ for-model/eventcollections/src/main/java/com/example/template/infra/EventCollectorViewHandler.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-import {{options.package}}.config.kafka.KafkaProcessor;
-import {{options.package}}.domain.*;
+import com.example.template.config.kafka.KafkaProcessor;
+import com.example.template.domain.EventCollector;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
@@ -21,29 +21,44 @@ public class EventCollectorViewHandler {
     @Autowired
     private EventCollectorRepository eventCollectorRepository;
 
+{{#boundedContexts}}
+    {{#each attached}}
+    {{#if (isEvent _type)}}
     @StreamListener(KafkaProcessor.INPUT)
-    public void whenOrderPlaced_then_CREATE_1(
-        @Payload OrderPlaced orderPlaced
+    public void when{{namePascalCase}}_then_CREATE_1(
+        @Payload {{namePascalCase}} {{nameCamelCase}}
     ) {
         try {
-            if (!orderPlaced.validate()) return;
+            if (!{{nameCamelCase}}.validate()) return;
 
             // view 객체 생성
             EventCollector eventCollector = new EventCollector();
             // view 객체에 이벤트의 Value 를 set 함
-            eventCollector.setType(orderPlaced.getEventType());
+            eventCollector.setType({{nameCamelCase}}.getEventType());
             eventCollector.setCorrelationKey(
-                String.valueOf(orderPlaced.getId())
+                String.valueOf({{nameCamelCase}}.getId())
             );
             ObjectMapper objectMapper = new ObjectMapper();
-            String jsonPayload = objectMapper.writeValueAsString(orderPlaced);
+            String jsonPayload = objectMapper.writeValueAsString({{nameCamelCase}});
             eventCollector.setPayload(jsonPayload);
-            eventCollector.setTimestamp(orderPlaced.getTimestamp());
+            eventCollector.setTimestamp({{nameCamelCase}}.getTimestamp());
             // view 레파지 토리에 save
             eventCollectorRepository.save(eventCollector);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    {{/if}}
+    {{/each}}
+{{/boundedContexts}}
     //>>> DDD / CQRS
 }
+<function>
+window.$HandleBars.registerHelper('isEvent', function (type) {
+    if (type.endsWith("Event")) {
+        return true;
+    } else {
+        return false;
+    }
+});
+</function>
